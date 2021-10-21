ed6465a55771e6b93fc2f5f6f4f03fdce6633f76
yosep <yosepalexsander9@gmail.com>
2021-10-21 10:29:45 +0700

    done navigation and routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 
 // import necessary object from react-router-dom
+import { Route, Switch, Link } from "react-router-dom";
 
 // import "pages" component here
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
 
 function App() {
   return (
     // define <BrowserRouter> as parent element
     <>
       <div>
-        {/* Setup navigation here */}
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+            <li>
+              <Link to="/signin">Signin</Link>
+            </li>
+          </ul>
+        </nav>
       </div>
-      {/* code <Switch> and <Route> below, according to path and component */}
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/signin" component={SignIn} />
+      </Switch>
     </>
   );
 }

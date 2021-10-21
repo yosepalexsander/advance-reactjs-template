5b912923bab04f99aa5e0e65a7cc0c7b9a359224
yosep <yosepalexsander9@gmail.com>
2021-10-21 11:29:44 +0700

    add back implementation

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,15 +1,16 @@
 import { Container, Form, Button } from "react-bootstrap";
 
+import { useHistory } from "react-router-dom";
 const Signin = () => {
+  const history = useHistory();
+
   return (
     <Container>
       <Form>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
+          <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
@@ -21,6 +22,7 @@ const Signin = () => {
         <Button variant="primary" type="submit">
           Submit
         </Button>
+        <Button onClick={() => history.goBack()}>Go Back</Button>
       </Form>
     </Container>
   );

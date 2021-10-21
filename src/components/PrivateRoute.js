ad5d85728cf5fbc17757789252e3e8c2542af80c
yosep <yosepalexsander9@gmail.com>
2021-10-21 11:00:20 +0700

    done private route

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,4 +1,16 @@
 // import necessary utility from rrd
-
+import { Redirect, Route } from "react-router-dom";
 
 // create component here
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  // create statement for validation (by hardcode first)
+  const isSignin = true;
+
+  return (
+    <>
+      <Route {...rest} render={(props) => (isSignin ? <Component {...props} /> : <Redirect to="/signin" />)} />
+    </>
+  );
+};
+
+export default PrivateRoute;
